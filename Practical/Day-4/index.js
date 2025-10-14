5c0ae9f636fde38f526c46081bd77df1107a01b4
anashali01 <saiyedanashali@gmail.com>
2025-10-14 10:49:56 +0530

    mongodb and postman connect

--- Practical/Day-4/index.js
+++ Practical/Day-4/index.js
@@ -1,17 +1,40 @@
 import express from "express";
 import dotenv from "dotenv";
-import {db} from "./config/db.js";
+import { db } from "./config/db.js";
+import { User } from "./models/user.modal.js";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8101;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/user/create', (req, res) => {
+    User.create(req.body)
+        .then((data) => {
+            return res.json(data);
+        })
+        .catch((error) => {
+            return res.json(error.message);
+        })
+});
+
+app.get('/user/getAllUser', (req, res) => {
+    User.find({})
+        .then((data) => {
+            return res.json(data)
+        })
+        .catch((error) => {
+            return res.json({ message: error.message });
+        })
+})
 
 //! Error Route
-app.listen(port,(err)=>{
-    if(!err){
-        console.log(`Server is running on localhost:${port}`);
-    }else{
+app.listen(port, (err) => {
+    if (!err) {
+        console.log(`Server is running on http://127.0.0.1:${port}`);
+    } else {
         console.log(`Error`);
     }
 }) 
