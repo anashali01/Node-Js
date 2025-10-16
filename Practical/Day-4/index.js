290dfacdff05cdaa6952a2b9253426076f55bdff
anashali01 <saiyedanashali@gmail.com>
2025-10-16 10:56:37 +0530

    rest API  setup

--- Practical/Day-4/index.js
+++ Practical/Day-4/index.js
@@ -9,7 +9,58 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8101;
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//! Main Page Render Route 
+app.get('/', (req, res) => {
+    return res.render('index');
+})
+
+//! Tables Page Render Route 
+app.get('/tables', (req, res) => {
+    return res.render('pages/tables');
+})
+
+//! Form basic Page Render Route 
+app.get('/form-basic', (req, res) => {
+    return res.render('pages/form-basic');
+})
+
+//! Login Page Render Route 
+app.get('/login', (req, res) => {
+    return res.render('pages/login');
+})
+
+//! Signup Page Render Route 
+app.get('/signup', (req, res) => {
+    return res.render('pages/signup');
+})
+
+//! Get Data From Form Route 
+app.post('/signup', (req, res) => {
+    console.log("data from signup", req.body);
+    fetch('http://127.0.0.1:6100/user/create', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req.body)
+    })
+        .then((rs) => {
+            return rs.json()
+        })
+        .then((data) => {
+            console.log(data);
+            return res.redirect('/login')
+        })
+        .catch((error) => {
+            console.log(`Error ${error.message}`);
+            res.redirect('/')
+        })
+})
 //! Create User Route
 app.post('/user/create', (req, res) => {
     User.create(req.body)
@@ -33,27 +84,27 @@ app.get('/user/getAllUser', (req, res) => {
 })
 
 //! Delete User Route
-app.get('/user/delete/:id' , (req , res)=>{
-    const {id} = req.params;
+app.get('/user/delete/:id', (req, res) => {
+    const { id } = req.params;
     User.findByIdAndDelete(id)
-    .then((data)=>{
-        return res.json({message : "User Deleted!",data})
-    })
-    .catch((error)=>{
-        return res.json({message : error.message})
-    })
-}) 
+        .then((data) => {
+            return res.json({ message: "User Deleted!", data })
+        })
+        .catch((error) => {
+            return res.json({ message: error.message })
+        })
+})
 
 //! Update User Route 
-app.post('/user/update/:id' , (req,res)=>{
-    const {id} = req.params;
-    User.findByIdAndUpdate(id,req.body)
-    .then((data)=>{
-        return res.json({message : "User Updated!"})
-    })
-    .catch((error)=>{
-        return res.json({message:error.message})
-    })
+app.post('/user/update/:id', (req, res) => {
+    const { id } = req.params;
+    User.findByIdAndUpdate(id, req.body)
+        .then((data) => {
+            return res.json({ message: "User Updated!" })
+        })
+        .catch((error) => {
+            return res.json({ message: error.message })
+        })
 })
 //! Error Route
 app.listen(port, (err) => {
